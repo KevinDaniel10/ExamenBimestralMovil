5a40c9e2b685e1f9997a589fc054e9046f2ca8da
KevinDaniel10 <150805852+KevinDaniel10@users.noreply.github.com>
2024-11-22 15:22:57 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'libro-perrito-app',
+  webDir: 'www'
+};
+
+export default config;
